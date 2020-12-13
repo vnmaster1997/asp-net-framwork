097bb52c578540fa2faf61dc39ff4a462b731c17
namnv1503 <namnv537@gmail.com>
2020-12-13 16:09:51 +0700

    add a new field

---
+++ MvcMovie/Migrations/Configuration.cs
@@ -0,0 +1,58 @@
+﻿namespace MvcMovie.Migrations
+{
+    using MvcMovie.Models;
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<MvcMovie.Models.MovieDBContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(MvcMovie.Models.MovieDBContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+            context.Movies.AddOrUpdate(i => i.Title,
+                new Movie
+                {
+                    Title = "When Harry Met Sally",
+                    ReleaseDate = DateTime.Parse("1989-1-11"),
+                    Genre = "Romantic Comedy",
+                    Rating = "PG",
+                    Price = 7.99M
+                },
+                new Movie
+                {
+                    Title = "Ghostbusters ",
+                    ReleaseDate = DateTime.Parse("1987-2-14"),
+                    Genre = "Comedy",
+                    Rating = "OG",
+                    Price = 8.99M
+                },
+                new Movie
+                {
+                    Title = "Ghostbusters 2",
+                    ReleaseDate = DateTime.Parse("1986-2-23"),
+                    Genre = "Comedy",
+                    Rating = "DG",
+                    Price = 9.99M
+                },
+                new Movie
+                {
+                    Title = "Rio Bravo",
+                    ReleaseDate = DateTime.Parse("1959-4-15"),
+                    Genre = "Western",
+                    Rating = "PG",
+                    Price = 3.99M
+                }
+            );
+        }
+    }
+}
